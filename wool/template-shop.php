1ae7efd139f2ddc56d01a06ae2d1103ba7f7c932
Levani1111 <papashvililevani@gmail.com>
2022-01-03 12:52:35 -0500

    add into theme woocommerce files to style easlit

--- wool/template-shop.php
+++ wool/template-shop.php
@@ -10,7 +10,6 @@ get_header(); ?>
     <div class="container">
          <div class="row">
             <div class="col-lg-12">
-    
                 <h1><?php the_title();?></h1>
                 <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
                     <?php the_content(); ?>
